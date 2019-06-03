023fec8a7a6a6d99f55841c1d87a37d0bb7654ac
Braam Beresford <braam@braam.email>
2019-06-02 19:20:35 -0700

    Ball cross screen

--- ball.sv
+++ ball.sv
@@ -1,44 +1,61 @@
 module ball #(parameter right_pad_x = 540)(input logic clk,
 	input logic reset_n,
 	input logic [9:0] right_paddle_pos,
-	output logic, 
-	output logic ball_y_pos [9:0],
-	output logic ball_x_pos [9:0]);
+	output logic [9:0] ball_y_pos ,
+	output logic [9:0] ball_x_pos );
 
-logic [9:0] next_pos;
-logic [2:0] x_velocity;
+logic [9:0] next_pos_x;
+logic [9:0] next_pos_y;
+logic [2:0] next_x_velocity;
+logic [2:0] next_y_velocity;
+logic neg_x_vel;
+logic next_neg_x_vel;
+
+logic signed [2:0] x_velocity;
 logic [2:0] y_velocity;
 
 //Make a block
-logic [17:0] q;
-always_ff @(posedge clk, negedge rst_n)
-	if(~rst_n) begin q<=0;end
+logic [25:0] q;
+always_ff @(posedge clk, negedge reset_n)
+	if(~reset_n) begin q<=0;end
 	else
 		begin 
 		q = q+1;
 		end
 
-always_ff @(posedge clk, negedge reset_n) begin 
+always_ff @(posedge q[20], negedge reset_n) begin 
 	if(~reset_n) begin
-		ball_x_pos <= 300;
+		ball_x_pos <= 10;
 		ball_y_pos <= 200;
-		x_velocity <= 5;
+		x_velocity <= 3'b111;
 		y_velocity <= 0;
+		neg_x_vel <= 0;
 	end 
 	else
 	begin
-		if(ball_x_pos+ x_velocity => right_pad_x && ball_y_pos =< right_paddle_pos + 20 && ball_y_pos => right_paddle_pos + 20) begin
-				ball_x_pos <= right_pad_x - 1;
-				ball_y_pos <= ball_y_pos + y_velocity;
-				x_velocity <= x_velocity * -1; //Make it bounce back
-			end
-		else begin
-			ball_x_pos <= ball_x_pos + x_velocity;
-			ball_y_pos <= ball_y_pos + y_velocity;
-		end
-
+		ball_y_pos <= next_pos_y;
+		ball_x_pos <= next_pos_x;
+//		x_velocity <= next_x_velocity;
+		neg_x_vel <= next_neg_x_vel;
 	end
 end
+
+
+always_comb
+	if(ball_x_pos+x_velocity >= right_pad_x && ball_y_pos <= right_paddle_pos + 20 && ball_y_pos >= right_paddle_pos - 20) begin
+					next_pos_x = right_pad_x - 1;
+					next_pos_y = ball_y_pos + y_velocity;
+//					next_x_velocity = x_velocity * -1; //Make it bounce back
+					next_neg_x_vel = (neg_x_vel == 0) ? 1: 0;
+				end
+	else begin
+		if(neg_x_vel) next_pos_x = ball_x_pos - x_velocity;
+		else next_pos_x = ball_x_pos + x_velocity;
+		next_neg_x_vel = neg_x_vel;
+		next_pos_y = ball_y_pos + y_velocity;
+		next_x_velocity = 3;
+		next_y_velocity = 3;
+		end
 
 
 endmodule // ball
--- pong.sv
+++ pong.sv
@@ -8,52 +8,67 @@ module pong (input logic masterCLK, output logic hsync,
 		logic toOutputMux;
 		logic [9:0] x_count;
 		logic [9:0] y_count;
-		
+		logic [9:0] ball_y_pos;
+		logic [9:0] ball_x_pos;
 
 		
 		clkDivider clkDiv(
-		.clk(masterCLK),
-		.reset_n(reset_n),
-		.newClk(clk)
-		);
+			.clk(masterCLK),
+			.reset_n(reset_n),
+			.newClk(clk)
+			);
 			
 		horizCounter horizCounter1(
-		.clk(clk),
-		.reset_n(reset_n),
-		.hsync(hsync),
-		.x_count(x_count)
-		);
+			.clk(clk),
+			.reset_n(reset_n),
+			.hsync(hsync),
+			.x_count(x_count)
+			);
 		
 		vertCounter vertCounter1(
-		.clk(clk),
-		.reset_n(reset_n),
-		.vsync(vsync),
-		.x_count(x_count),
-		.y_count(y_count)
-		);
+			.clk(clk),
+			.reset_n(reset_n),
+			.vsync(vsync),
+			.x_count(x_count),
+			.y_count(y_count)
+			);
 		
 		assign toOutputMux = (x_count < 640) && (y_count < 480);
 		assign blue = 0;
-		assign red = 0;
+		// assign red = 0;
 		assign greenTest = (x_count < 550 && x_count > 540) ?
-										((y_pos+20 >= y_count && y_pos-20 <= y_count ) ? 4'b111: 0): 0 ;
-	
+							((y_count <= y_pos+20  &&  y_count >= y_pos-20) ? 4'b111: 0): 0 ;
+
+		assign redTest =  (x_count < ball_x_pos + 5 && x_count > ball_x_pos-5)?  
+							(( y_count <= ball_y_pos+5 && y_count >= ball_y_pos-5) ? 4'b111: 0): 0;
 		
 		enableMux greenMux(
-		.a(greenTest),
-		.controlSig(toOutputMux),
-		.d(green)
-		);
+			.a(greenTest),
+			.controlSig(toOutputMux),
+			.d(green)
+			);
+
+		enableMux reMux(
+			.a(redTest),
+			.controlSig(toOutputMux),
+			.d(red)
+			);
 		
 		
 		rightPadd paddle(
-		.clk(clk),
-		.rst_n(reset_n),
-		.mv_up(up_switch),
-		.y_pos(y_pos)
-		);
-		
-		
+			.clk(clk),
+			.rst_n(reset_n),
+			.mv_up(up_switch),
+			.y_pos(y_pos)
+			);
+
+		ball ball1(
+			.clk(clk),
+			.reset_n(reset_n),
+			.right_paddle_pos(y_pos),
+			.ball_y_pos(ball_y_pos),
+			.ball_x_pos(ball_x_pos)
+			);
 		
 		
 endmodule
